974c37d31aad3eac08a1a24cf218c11e767b0483
awd <awd>
2019-03-15 13:23:27 -0400

    shiiiet

--- auctions/views.py
+++ auctions/views.py
@@ -43,7 +43,7 @@ def new_auction(request):
                 form.instance.underlying_asset = a_class
             form.instance.asset_class = a_class
             form.instance.seller = request.user
-            form.instance.high_bid = form.instance.unit_price * 1.01
+            form.instance.high_bid = round(form.instance.unit_price * 1.01, 2)
       
             form.save()
 
