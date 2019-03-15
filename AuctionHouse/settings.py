b415dd7460ec7add696a1ec4a03af6ce450d6f1e
awd <awd>
2019-03-15 13:13:22 -0400

    pls god

--- AuctionHouse/settings.py
+++ AuctionHouse/settings.py
@@ -87,11 +87,11 @@ WSGI_APPLICATION = 'AuctionHouse.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default=config('DATABASE_URL')
-#     )
-# }
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    )
+}
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # DATABASES = {
 #     'default': {
