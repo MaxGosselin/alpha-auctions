cf759399203f6b6444a214bd0ec968c39bf4ee34
awd <awd>
2019-03-15 13:11:48 -0400

    fucking typo

--- AuctionHouse/settings.py
+++ AuctionHouse/settings.py
@@ -17,7 +17,7 @@ import dj_database_url
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))d
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 AUTH_USER_MODEL = 'homepage.User'
 # Quick-start development settings - unsuitable for production
